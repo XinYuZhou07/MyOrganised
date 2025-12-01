fa2f6e11876c0b7544fa145e45dcab8d105bbc18
Gaia <zhoxny07t60d912l@isisbernocchi.edu.it>
2025-12-01 15:46:32 +0100

    js store

--- frontend/JS/store.js
+++ frontend/JS/store.js
@@ -1,47 +1,13 @@
-let buttonW = document.querySelector(".w")
-let buttonA = document.querySelector(".a")
-let buttonS = document.querySelector(".s")
-let buttonD = document.querySelector(".d")
-let buttonJ = document.querySelector(".j")
-let buttonK = document.querySelector(".k")
-let buttonL = document.querySelector(".l")
+let filtersBtn = document.querySelectorAll(".filter-button");
 
-buttonW.addEventListener("click", function(){
-    var tmp = new Audio("sounds/tom-1.mp3")
-    tmp.play()
-})
-buttonA.addEventListener("click", function(){
-    var tmp = new Audio("sounds/tom-2.mp3")
-    tmp.play()
-})
-buttonS.addEventListener("click", function(){
-    var tmp = new Audio("sounds/tom-3.mp3")
-    tmp.play()
-})
-buttonD.addEventListener("click", function(){
-    var tmp = new Audio("sounds/tom-4.mp3")
-    tmp.play()
-})
-buttonJ.addEventListener("click", function(){
-    var tmp = new Audio("sounds/crash.mp3")
-    tmp.play()
-})
-buttonK.addEventListener("click", function(){
-    var tmp = new Audio("sounds/kick-bass.mp3")
-    tmp.play()
-})
-buttonL.addEventListener("click", function(){
-    var tmp = new Audio("sounds/snare.mp3")
-    tmp.play()
-})
+filtersBtn.forEach(element => {
+    element.addEventListener("click", function(){
+        if(element.classList.contains("filter-active")){
+            element.classList.remove("filter-active");
+            console.log(element.innerHTML);
+        }else{
+            element.classList.add("filter-active");
+        }
+    })
+});
 
-//let numberOfBtn = document.querySelector(".drum")
-
-
-buttonW.addEventListener("click", suono("tom-1.mp3"))
-
-
-function suono(event){
-    var suono = new Audio("sounds/"+event)
-    suono.play()
-}
