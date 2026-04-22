f3870682e2ccf64aafc3bfba5ff3999a86e2cec5
XinYuZhou07 <zhougaia07@gmail.com>
2026-04-22 11:35:17 +0200

    DB

---
+++ myorganized.sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Apr 22, 2026 alle 11:32
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `myorganized`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `idUsr` int(11) DEFAULT NULL,
+  `idProp` int(11) DEFAULT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `desciz` varchar(100) DEFAULT NULL,
+  `position` text DEFAULT NULL,
+  `start` datetime DEFAULT NULL,
+  `end` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `proposals`
+--
+
+CREATE TABLE `proposals` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL DEFAULT '0',
+  `idRoutine` int(11) NOT NULL DEFAULT 0,
+  `idState` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `routines`
+--
+
+CREATE TABLE `routines` (
+  `id` int(11) NOT NULL,
+  `idOwner` int(11) NOT NULL DEFAULT 0,
+  `name` varchar(50) DEFAULT NULL,
+  `descriz` varchar(100) DEFAULT NULL,
+  `idTag` int(11) NOT NULL DEFAULT 0,
+  `price` float NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `state`
+--
+
+CREATE TABLE `state` (
+  `id` int(11) NOT NULL,
+  `desciz` varchar(50) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL,
+  `descriz` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `psw` varchar(100) DEFAULT NULL,
+  `idState` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `surname` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `usr-routines`
+--
+
+CREATE TABLE `usr-routines` (
+  `id` int(11) NOT NULL,
+  `idUsr` int(11) DEFAULT NULL,
+  `idRoutine` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `proposals`
+--
+ALTER TABLE `proposals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `routines`
+--
+ALTER TABLE `routines`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `state`
+--
+ALTER TABLE `state`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `usr-routines`
+--
+ALTER TABLE `usr-routines`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `proposals`
+--
+ALTER TABLE `proposals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `routines`
+--
+ALTER TABLE `routines`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `state`
+--
+ALTER TABLE `state`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `usr-routines`
+--
+ALTER TABLE `usr-routines`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
